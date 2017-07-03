94839fb2141610d97c7c5810346797d44a403f57
amete <serhanmete@gmail.com>
2017-07-03 13:11:57 +0200

    adding more features

--- read_xml.R
+++ read_xml.R
@@ -24,144 +24,8 @@ xmltop <- xmlRoot(data)
 
 # Functions that draws the NSW based on the XML input
 
-# Large sector - pivot
-draw_large_sectors <- function(xmltop, firstOnly = F) {
-    #
-    # Large Sectors
-    #
-    
-    # sTG1-QL1P
-    sWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[17]],"sWidth"))
-    lWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[17]],"lWidth"))
-    Length  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[17]],"Length"))
-    yCutout <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[17]],"yCutout"))
-    radius  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[18]],"radius"))
-    x1 <- c(sWidth*0.5,-sWidth*0.5,-lWidth*0.5,lWidth*0.5)
-    y1 <- c(0.,0.,Length,Length)
-    y1 <- radius+y1-0.5*Length
-    # sTG1-QL2P
-    sWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[19]],"sWidth"))
-    lWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[19]],"lWidth"))
-    Length  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[19]],"Length"))
-    yCutout <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[19]],"yCutout"))
-    radius  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[20]],"radius"))
-    x2 <- c(sWidth*0.5,-sWidth*0.5,-lWidth*0.5,lWidth*0.5)
-    y2 <- c(0.,0.,Length,Length)
-    y2 <- radius+y2-0.5*Length
-    # sTG1-QL3P
-    sWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[21]],"sWidth"))
-    lWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[21]],"lWidth"))
-    Length  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[21]],"Length"))
-    yCutout <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[21]],"yCutout"))
-    radius  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[22]],"radius"))
-    x3 <- c(sWidth*0.5,-sWidth*0.5,-lWidth*0.5,-lWidth*0.5,lWidth*0.5,lWidth*0.5)
-    y3 <- c(0.,0.,Length-yCutout,Length,Length,Length-yCutout)
-    y3 <- radius+y3-0.5*Length
-    
-    # Draw
-    for(i in seq(1,16,by=2)) {
-        if(firstOnly && i>1 ) break
-        angle <- 2*pi/16*(i-1)
-        # Draw sTG1-QS1P
-        M1 <- cbind(x1,y1)
-        M1R <- rotate(M1,angle)
-        points(M1R[,1], M1R[,2], col="blue", cex = 0.1, pch=19)
-        polygon(M1R[,1],M1R[,2], col=rgb(0.54, 0.81, 0.94, 0.5)) 
-        # Draw sTG1-QS2P
-        M2 <- cbind(x2,y2)
-        M2R <- rotate(M2,angle)
-        points(M2R[,1], M2R[,2], col="green", cex = 0.1, pch=19)
-        #polygon(M2R[,1],M2R[,2], col=rgb(1, 0.65, 0, 0.5))
-        polygon(M2R[,1],M2R[,2], col=rgb(0.54, 0.81, 0.94, 0.5))
-        # Draw sTG1-QS3P
-        M3 <- cbind(x3,y3)
-        M3R <- rotate(M3,angle)
-        points(M3R[,1], M3R[,2], col="red", cex = 0.1, pch=19)
-        polygon(M3R[,1],M3R[,2], col=rgb(0.54, 0.81, 0.94, 0.5))
-    }
-}
-
-# Small sector - pivot
-draw_small_sectors <- function(xmltop) {
-    #
-    # Small Sectors
-    #
-    
-    # Draw sTG1-QS1P
-    sWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[3]],"sWidth"))
-    lWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[3]],"lWidth"))
-    Length  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[3]],"Length"))
-    yCutout <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[3]],"yCutout"))
-    radius  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[4]],"radius"))
-    x1 <- c(sWidth*0.5,-sWidth*0.5,-lWidth*0.5,lWidth*0.5)
-    y1 <- c(0.,0.,Length,Length)
-    y1 <- radius+y1-0.5*Length
-    # Draw sTG1-QS2P
-    sWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[5]],"sWidth"))
-    lWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[5]],"lWidth"))
-    Length  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[5]],"Length"))
-    yCutout <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[5]],"yCutout"))
-    radius  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[6]],"radius"))
-    x2 <- c(sWidth*0.5,-sWidth*0.5,-lWidth*0.5,lWidth*0.5)
-    y2 <- c(0.,0.,Length,Length)
-    y2 <- radius+y2-0.5*Length
-    # Draw sTG1-QS3P
-    sWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[7]],"sWidth"))
-    lWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[7]],"lWidth"))
-    Length  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[7]],"Length"))
-    yCutout <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[7]],"yCutout"))
-    radius  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[8]],"radius"))
-    yCutout <- 0
-    x3 <- c(sWidth*0.5,-sWidth*0.5,-lWidth*0.5,lWidth*0.5)
-    y3 <- c(0.,0.,Length,Length)
-    y3 <- radius+y3-0.5*Length
-    
-    # Small Sectors
-    for(i in seq(1,16,by=2)) {
-        angle <- 2*pi/16*(i)
-        # Draw sTG1-QS1P
-        M1 <- cbind(x1,y1)
-        M1R <- rotate(M1,angle)
-        points(M1R[,1], M1R[,2], col="blue", cex = 0.1, pch=19)
-        polygon(M1R[,1],M1R[,2], col=rgb(1, 0.65, 0, 0.5))
-        # Draw sTG1-QS2P
-        M2 <- cbind(x2,y2)
-        M2R <- rotate(M2,angle)
-        points(M2R[,1], M2R[,2], col="green", cex = 0.1, pch=19)
-        polygon(M2R[,1],M2R[,2], col=rgb(0.54, 0.81, 0.94, 0.5))
-        # Draw sTG1-QS3P
-        M3 <- cbind(x3,y3)
-        M3R <- rotate(M3,angle)
-        points(M3R[,1], M3R[,2], col="red", cex = 0.1, pch=19)
-        polygon(M3R[,1],M3R[,2], col=rgb(1, 0.65, 0, 0.5))
-    }
-}
-
-# Draw the entire NSW
-draw_nsw <- function(xmltop) {
-
-    # First plot the canvas
-    pdf("SingleWheelPivot_r19.pdf")
-    plot(0, 0, xlim = c(-5000,5000), ylim = c(-5000,5000), type="n",
-         main = "ATLAS NSW Layout", xlab = "x [mm]", ylab = "y [mm]")
-    grid(nx = 30)
-
-    # Plot the large sectors
-    draw_large_sectors(xmltop)
-    
-    # Plot the small sectors
-    draw_small_sectors(xmltop)
-
-    # Close the device    
-    dev.off()
-}
-
-if(draw.nsw) {
-    draw_nsw(xmltop)
-}
-
 # Draw pads
-draw_pads <- function(xmltop,index,layer=1,col="black") {
+draw_pads <- function(xmltop,index,layer=1,col="black",angle=0.) {
 
     # index corresponds to a specific module, i.e. sTG1-QL1P etc.
     sWidth      <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[index]],"sWidth"))
@@ -181,82 +45,296 @@ draw_pads <- function(xmltop,index,layer=1,col="black") {
     firstPadRow <- as.numeric(strsplit(xmlGetAttr(xmltop[[3]][[1]][[index]][[1]],"firstPadRow"),";")[[1]][[layer]])
     firstPadPhiDivision_A <- as.numeric(strsplit(xmlGetAttr(xmltop[[3]][[1]][[index]][[1]],"firstPadPhiDivision_A"),";")[[1]][[layer]])
     PadPhiShift_A <- as.numeric(strsplit(xmlGetAttr(xmltop[[3]][[1]][[index]][[1]],"PadPhiShift_A"),";")[[1]][[layer]])
-    
+
     # Beginning of the first pad :  radius+y1-0.5*Length
     offset <- radius-0.5*Length
     tanT <- (0.5*(lWidth-sWidth))/(Length-yCutout)
-    lines(c(-0.5*sWidth,0.5*sWidth),c(offset,offset),col=col)
-    lines(firstPadH*tanT*c(-1,1)+c(-0.5*sWidth,0.5*sWidth),c(offset+firstPadH,offset+firstPadH),col=col)
+    # Bottom corners
+    xx <- c(-0.5*sWidth,0.5*sWidth)
+    yy <- c(offset,offset)
+    # Rotate if necessary
+    if(angle!=0) {
+        M1 <- cbind(xx,yy)
+        M1R <- rotate(M1,angle)
+        xx <- M1R[,1]
+        yy <- M1R[,2]
+    }
+    lines(xx,yy,col=col)
+    # First Pad needs special treatment
+    xx <- firstPadH*tanT*c(-1,1)+c(-0.5*sWidth,0.5*sWidth)
+    yy <- c(offset+firstPadH,offset+firstPadH)
+    # Rotate if necessary
+    if(angle!=0) {
+        M1 <- cbind(xx,yy)
+        M1R <- rotate(M1,angle)
+        xx <- M1R[,1]
+        yy <- M1R[,2]
+    }
+    lines(xx,yy,col=col)
     # Eta-lines
     for(i in seq(2,nPadH,by=1)) {
         value <- firstPadH+(i-1)*padH
         if(value > Length) value <- Length
         if(value < Length-yCutout) {
-            lines(value*tanT*c(-1,1)+c(-0.5*sWidth,0.5*sWidth),
-                c(offset+value,offset+value),col=col) # The last row spills over!
+            xx <- value*tanT*c(-1,1)+c(-0.5*sWidth,0.5*sWidth)
+            yy <- c(offset+value,offset+value)
         } else {
-            lines(c(-0.5*lWidth,0.5*lWidth),
-                  c(offset+value,offset+value),col=col) # The last row spills over!            
+            xx <- c(-0.5*lWidth,0.5*lWidth)
+            yy <- c(offset+value,offset+value)
         }
+        # Rotate if necessary
+        if(angle!=0) {
+            M1 <- cbind(xx,yy)
+            M1R <- rotate(M1,angle)
+            xx <- M1R[,1]
+            yy <- M1R[,2]
+        }
+        # Actual drawing
+        lines(xx,yy,col=col) # The last row spills over!
     }
     # Phi-lines
+    # Angle is measured from y-axis in counter-clockwise direction - needs special attention!
+    #firstPadPhiDivision_A <- firstPadPhiDivision_A+pi/2.
     tanP <- tan(firstPadPhiDivision_A/180*pi)
-    lines(tanP*c(offset,offset+Length),c(offset,offset+Length),col=col)
-    for(i in seq(1,nPadPhi-2,by=1)) {
-        tanP <- tan((firstPadPhiDivision_A+i*anglePadPhi)/180*pi)
-        lines(tanP*c(offset,offset+Length),c(offset,offset+Length),col=col)
+    xx <- tanP*c(offset,offset+Length)
+    yy <- c(offset,offset+Length)
+    # Rotate if necessary
+    if(angle!=0) {
+        M1 <- cbind(xx,yy)
+        M1R <- rotate(M1,angle)
+        xx <- M1R[,1]
+        yy <- M1R[,2]
+    }
+    # Actual drawing
+    lines(xx,yy,col=col)
+    if(nPadPhi>2) {
+        for(i in seq(1,nPadPhi-2,by=1)) {
+            tanP <- tan((firstPadPhiDivision_A+i*anglePadPhi)/180*pi)
+            xx <- tanP*c(offset,offset+Length)
+            yy <- c(offset,offset+Length)
+            # Rotate if necessary
+            if(angle!=0) {
+                M1 <- cbind(xx,yy)
+                M1R <- rotate(M1,angle)
+                xx <- M1R[,1]
+                yy <- M1R[,2]
+            }
+            # Actual drawing
+            lines(xx,yy,col=col)
+        }
     }
 }
 
+# Get radius for a given eta and z
 get_radius <- function(eta,z) {
     theta <- 2*atan(exp(-eta))
     return(z*tan(theta))
 }
 
+# Main control function for drawing the pads
 if(draw.pads) {
-    
+ 
+    # Draw eta lines
+    draw.eta.lines <- F
+       
     # First plot the canvas
     pdf("LargeSectorPivot_L1_r19.pdf")
     plot(0, 0, xlim = c(-2000,2000), ylim = c(500,5000), type="n",
          main = "ATLAS NSW Layout (Large Pivot - Layer 1)", xlab = "x [mm]", ylab = "y [mm]")
     grid(nx = 30)
+    # pdf("SmallSectorPivot_L3_r19.pdf")
+    # plot(0, 0, xlim = c(-2000,2000), ylim = c(500,5000), type="n",
+    #      main = "ATLAS NSW Layout (Small Pivot - Layer 3)", xlab = "x [mm]", ylab = "y [mm]")
+    # grid(nx = 30)
     
     # Plot the large sectors
-    draw_large_sectors(xmltop,T)
-    
-    # Draw the pads now 
-    # - first layer only for the time being 
-    # - also ignore the frames for the time-being
-
-    # sTG1-QL1P
-    draw_pads(xmltop,17,1)
-    #draw_pads(xmltop,17,3,col="red")
-    
-    # sTG1-QL2P
-    draw_pads(xmltop,19,1)
-    #draw_pads(xmltop,19,3,col="red")
-    
-    # sTG1-QL3P
-    draw_pads(xmltop,21,1)
-    #draw_pads(xmltop,21,3,col="red")
+    draw_large_sectors(xmltop,firstOnly = T, with.pads = T)
+    # draw_small_sectors(xmltop,firstOnly = T, with.pads = T, layer = 3)
     
     # Draw eta circles
-    require(plotrix)
-    r.1 <- get_radius(1.45,7474.) # Large Pivot
-    r.2 <- get_radius(1.9,7474.) # Large Pivot
-    r.3 <- get_radius(2.4,7474.) # Large Pivot
-    r.4 <- get_radius(3.0,7474.) # Large Pivot
-    draw.circle(0,0,radius=r.1,border="red")
-    text(x = 1750, y=4350., labels = expression(paste(eta," = 1.45")), col = "red")
-    draw.circle(0,0,radius=r.2,border="red")
-    text(x = 1500, y=2500., labels = expression(paste(eta," = 1.90")), col = "red")
-    draw.circle(0,0,radius=r.3,border="red")
-    text(x = 1050, y=1500., labels = expression(paste(eta," = 2.40")), col = "red")
-    draw.circle(0,0,radius=r.4,border="red")
-    text(x =  750, y= 750., labels = expression(paste(eta," = 3.00")), col = "red")
-    
+    if(draw.eta.lines) {
+        require(plotrix)
+        r.1 <- get_radius(1.45,7474.) # w.r.t. Large Pivot
+        r.2 <- get_radius(1.90,7474.) # w.r.t. Large Pivot
+        r.3 <- get_radius(2.40,7474.) # w.r.t. Large Pivot
+        r.4 <- get_radius(3.00,7474.) # w.r.t. Large Pivot
+        draw.circle(0,0,radius=r.1,border="red")
+        text(x = 1750, y=4350., labels = expression(paste(eta," = 1.45")), col = "red")
+        draw.circle(0,0,radius=r.2,border="red")
+        text(x = 1500, y=2500., labels = expression(paste(eta," = 1.90")), col = "red")
+        draw.circle(0,0,radius=r.3,border="red")
+        text(x = 1050, y=1500., labels = expression(paste(eta," = 2.40")), col = "red")
+        draw.circle(0,0,radius=r.4,border="red")
+        text(x =  750, y= 750., labels = expression(paste(eta," = 3.00")), col = "red")
+    }
+        
     # Close the device    
     dev.off()   
+}
+
+# Module corners
+get_module_corners <- function(xmltop,idx) {
+    # Get the module corners and return
+    sWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[idx]],"sWidth"))
+    lWidth  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[idx]],"lWidth"))
+    Length  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[idx]],"Length"))
+    yCutout <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[idx]],"yCutout"))
+    radius  <- as.numeric(xmlGetAttr(xmltop[[3]][[1]][[idx+1]],"radius"))
+    if(yCutout>0) {
+        x <- c(sWidth*0.5,-sWidth*0.5,-lWidth*0.5,-lWidth*0.5,lWidth*0.5,lWidth*0.5)
+        y <- c(0.,0.,Length-yCutout,Length,Length,Length-yCutout)
+        y <- radius+y-0.5*Length
+    } else {
+        x <- c(sWidth*0.5,-sWidth*0.5,-lWidth*0.5,lWidth*0.5)
+        y <- c(0.,0.,Length,Length)
+        y <- radius+y-0.5*Length  
+    }
+    return(list(x=x,y=y))
+}
+
+# Large sector - pivot
+draw_large_sectors <- function(xmltop, firstOnly = F, with.pads = F, layer = 1) {
+    #
+    # Large Sectors
+    #
+    
+    # sTG1-QL1P
+    corners <- get_module_corners(xmltop,17)
+    x1 <- corners$x
+    y1 <- corners$y
+    # sTG1-QL2P
+    corners <- get_module_corners(xmltop,19)
+    x2 <- corners$x
+    y2 <- corners$y
+    # sTG1-QL3P
+    corners <- get_module_corners(xmltop,21)
+    x3 <- corners$x
+    y3 <- corners$y
+    
+    # Draw
+    for(i in seq(1,16,by=2)) {
+        if(firstOnly && i>1 ) break
+        angle <- 2*pi/16*(i-1)
+        # Draw sTG1-QS1P
+        # Draw sTG1-QS1P
+        # Draw sTG1-QS1P
+        M1 <- cbind(x1,y1)
+        M1R <- rotate(M1,angle)
+        # points(M1R[,1], M1R[,2], col="blue", cex = 0.1, pch=19)
+        polygon(M1R[,1],M1R[,2], col=rgb(0.54, 0.81, 0.94, 0.5)) 
+        # Pads
+        if(with.pads) {
+            draw_pads(xmltop,index=17,layer=layer,col="black",angle=angle)
+        }
+        # Draw sTG1-QS2P
+        # Draw sTG1-QS2P
+        # Draw sTG1-QS2P
+        M2 <- cbind(x2,y2)
+        M2R <- rotate(M2,angle)
+        # points(M2R[,1], M2R[,2], col="green", cex = 0.1, pch=19)
+        polygon(M2R[,1],M2R[,2], col=rgb(1, 0.65, 0, 0.5))
+        #polygon(M2R[,1],M2R[,2], col=rgb(0.54, 0.81, 0.94, 0.5))
+        # Pads
+        if(with.pads) {
+            draw_pads(xmltop,index=19,layer=layer,col="black",angle=angle)
+        }
+        # Draw sTG1-QS2P
+        # Draw sTG1-QS2P
+        # Draw sTG1-QS2P
+        M3 <- cbind(x3,y3)
+        M3R <- rotate(M3,angle)
+        # points(M3R[,1], M3R[,2], col="red", cex = 0.1, pch=19)
+        polygon(M3R[,1],M3R[,2], col=rgb(0.54, 0.81, 0.94, 0.5))
+        # Pads
+        if(with.pads) {
+            draw_pads(xmltop,index=21,layer=layer,col="black",angle=angle)
+        }
+    }
+}
+
+# Small sector - pivot
+draw_small_sectors <- function(xmltop, firstOnly = F, with.pads = F, layer = 1) {
+    #
+    # Small Sectors
+    #
+    
+    # Draw sTG1-QS1P
+    corners <- get_module_corners(xmltop,3)
+    x1 <- corners$x
+    y1 <- corners$y
+    # Draw sTG1-QS2P
+    corners <- get_module_corners(xmltop,5)
+    x2 <- corners$x
+    y2 <- corners$y
+    # Draw sTG1-QS3P
+    corners <- get_module_corners(xmltop,7)
+    x3 <- corners$x
+    y3 <- corners$y
+    
+    # Small Sectors
+    for(i in seq(1,16,by=2)) {
+        if(firstOnly && i>1 ) break
+        angle <- 2*pi/16*(i)
+        if(firstOnly) angle <- 0 # Don't rotate
+        # Draw sTG1-QS1P
+        # Draw sTG1-QS1P
+        # Draw sTG1-QS1P
+        M1 <- cbind(x1,y1)
+        M1R <- rotate(M1,angle)
+        # points(M1R[,1], M1R[,2], col="blue", cex = 0.1, pch=19)
+        # polygon(M1R[,1],M1R[,2], col=rgb(1, 0.65, 0, 0.5))
+        polygon(M1R[,1],M1R[,2], col=rgb(0.54, 0.81, 0.94, 0.5))
+        # Pads
+        if(with.pads) {
+            draw_pads(xmltop,index=3,layer=layer,col="black",angle=angle)
+        }
+        # Draw sTG1-QS2P
+        # Draw sTG1-QS2P
+        # Draw sTG1-QS2P
+        M2 <- cbind(x2,y2)
+        M2R <- rotate(M2,angle)
+        # points(M2R[,1], M2R[,2], col="green", cex = 0.1, pch=19)
+        polygon(M2R[,1],M2R[,2], col=rgb(0.54, 0.81, 0.94, 0.5))
+        # Pads
+        if(with.pads) {
+            draw_pads(xmltop,index=5,layer=layer,col="black",angle=angle)
+        }
+        # Draw sTG1-QS3P
+        # Draw sTG1-QS3P
+        # Draw sTG1-QS3P
+        M3 <- cbind(x3,y3)
+        M3R <- rotate(M3,angle)
+        # points(M3R[,1], M3R[,2], col="red", cex = 0.1, pch=19)
+        # polygon(M3R[,1],M3R[,2], col=rgb(1, 0.65, 0, 0.5))
+        polygon(M3R[,1],M3R[,2], col=rgb(0.54, 0.81, 0.94, 0.5))
+        # Pads
+        if(with.pads) {
+            draw_pads(xmltop,index=7,layer=layer,col="black",angle=angle)
+        }
+    }
+}
+
+# Draw the entire NSW
+draw_nsw <- function(xmltop) {
+    
+    # First plot the canvas
+    pdf("SingleWheelPivot_r19.pdf")
+    plot(0, 0, xlim = c(-5000,5000), ylim = c(-5000,5000), type="n",
+         main = "ATLAS NSW Layout", xlab = "x [mm]", ylab = "y [mm]")
+    grid(nx = 30)
+    
+    # Plot the large sectors
+    draw_large_sectors(xmltop, with.pads = T) 
+    
+    # Plot the small sectors
+    draw_small_sectors(xmltop, with.pads = T) 
+    
+    # Close the device    
+    dev.off()
+}
+
+if(draw.nsw) {
+    draw_nsw(xmltop)
 }
 
 # 
